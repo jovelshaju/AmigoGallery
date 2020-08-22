69234a7a0994af079c18443d3e7588168b743e4c
Jovel Shaju <jovelshaju@gmail.com>
2020-08-22 21:25:38 +0530

    The Pages are Donegit add .

--- amigos/views.py
+++ amigos/views.py
@@ -1,19 +1,41 @@
 import os
 import random
+from django.urls import reverse 
+from django import forms
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
+from django import forms
+
+class LoginForm(forms.Form):
+   name = forms.CharField(max_length = 100)
 
 def index(request):
-    name = 'feny'
-    return HttpResponse(os.listdir(f'./amigos/static/img/{name}'))
+    if request.method == "POST":
+      MyLoginForm = LoginForm(request.POST)
+      
+      if MyLoginForm.is_valid():
+        name = MyLoginForm.cleaned_data['name']
+        return HttpResponseRedirect(reverse("index")+name)
+
+    else:
+        return render(request, "home/index.html")         
 
 def amigo(response ,name):
-    photos = os.listdir(f'./amigos/static/img/{name}')
-    photos +=photos[:]
-    random.shuffle(photos)
-    for i in range(len(photos)):
-        photos[i] = 'img/' + name + '/' + photos[i]
-    return render(response, "amigo/index.html", {
-        "name":name.lower(),
-        "photos": photos
-    })
+    if(name.isalpha()):
+        name = name.lower()
+        if(os.path.exists(f'./amigos/static/amigo/img/{name}')):
+            photos = os.listdir(f'./amigos/static/amigo/img/{name}')
+            photos +=photos[:]
+            random.shuffle(photos)
+            for i in range(len(photos)):
+                photos[i] = 'amigo/img/' + name + '/' + photos[i]
+            return render(response, "amigo/index.html", {
+                "name":name,
+                "photos": photos
+            })
+        else:
+            return render(response, "404/index.html", {
+                "name":name
+            })
+    else:
+        return HttpResponseRedirect(reverse('index'))
