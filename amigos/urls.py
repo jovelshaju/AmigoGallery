99b51ca7df172a20de7988d0e4bef99ce0d6b138
Jovel Shaju <jovelshaju@gmail.com>
2020-08-22 17:16:54 +0530

    Initial Page Created

--- amigos/urls.py
+++ amigos/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:name>", views.greet, name="greet")
+    path("<str:name>", views.amigo, name="amigo")
 ]
--- amigos/views.py
+++ amigos/views.py
@@ -1,5 +1,19 @@
+import os
+import random
 from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world")
+    name = 'feny'
+    return HttpResponse(os.listdir(f'./amigos/static/img/{name}'))
+
+def amigo(response ,name):
+    photos = os.listdir(f'./amigos/static/img/{name}')
+    photos +=photos[:]
+    random.shuffle(photos)
+    for i in range(len(photos)):
+        photos[i] = 'img/' + name + '/' + photos[i]
+    return render(response, "amigo/index.html", {
+        "name":name.lower(),
+        "photos": photos
+    })
